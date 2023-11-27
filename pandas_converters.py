96cd4b4875be10714d61c85c4c81f4dd755aebdf
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-27 21:28:11 +0100

    csv and json data collection finished

--- pandas_converters.py
+++ pandas_converters.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import xml.etree.ElementTree as ET
 
 
 def parse_children(children_str):
@@ -13,3 +14,30 @@ def parse_children(children_str):
             children_list.append({'name': name, 'age': int(age.strip('()'))})
 
     return children_list
+
+
+def xml_to_dataframe(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+
+def get_dataframe_from_multiple_json_files(json_files):
+    all_json_dataframes = []
+    for json_file in json_files:
+        json_dataframe = pd.read_json(json_file)
+        all_json_dataframes.append(json_dataframe)
+    final_dataframe = pd.concat(all_json_dataframes, ignore_index=True)
+    return final_dataframe
+
+
+def get_dataframe_from_multiple_csv_files(csv_files):
+    all_csv_dataframes = []
+    for csv_file in csv_files:
+        csv_dataframe = pd.read_csv(csv_file, delimiter=';', converters={'children': parse_children})
+        all_csv_dataframes.append(csv_dataframe)
+    final_dataframe = pd.concat(all_csv_dataframes, ignore_index=True)
+    return final_dataframe
+
+
+def get_dataframe_from_multiple_xml_files(xml_files):
+    pass
--- script.py
+++ script.py
@@ -1,11 +1,24 @@
 import pandas as pd
-from pandas_converters import parse_children
+from pandas_converters import parse_children, get_dataframe_from_multiple_json_files, \
+    get_dataframe_from_multiple_csv_files
 from file_searcher import get_file_list_deep
+import pandas as pd
 
 csv_files = get_file_list_deep('data', '.csv')
 xml_files = get_file_list_deep('data', '.xml')
 json_files = get_file_list_deep('data', '.json')
 
+csv_dataframe = get_dataframe_from_multiple_csv_files(csv_files)
+# xml_dataframes =
+json_dataframe = get_dataframe_from_multiple_json_files(json_files)
+
+# Concatenate all dataframes
+# all_dataframes = csv_dataframes + xml_dataframes + [json_dataframe]
+
+# Merge dataframes, drop duplicates, and handle telephone numbers
+# merged_df = pd.concat(all_dataframes, ignore_index=True)
+
+print(csv_dataframe)
 
 # df = pd.read_csv('data/a/b/users_1.csv', delimiter=';', converters={'children': parse_children})
 
