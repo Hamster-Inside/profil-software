c8f22a910970721f33f2a0ebcc526d3dfa20ef57
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-27 21:31:34 +0100

    separated pandas magic

--- script.py
+++ script.py
@@ -1,16 +1,14 @@
 import pandas as pd
-from pandas_converters import parse_children, get_dataframe_from_multiple_json_files, \
-    get_dataframe_from_multiple_csv_files
+import pandas_magic
 from file_searcher import get_file_list_deep
-import pandas as pd
 
 csv_files = get_file_list_deep('data', '.csv')
 xml_files = get_file_list_deep('data', '.xml')
 json_files = get_file_list_deep('data', '.json')
 
-csv_dataframe = get_dataframe_from_multiple_csv_files(csv_files)
-# xml_dataframes =
-json_dataframe = get_dataframe_from_multiple_json_files(json_files)
+csv_dataframe = pandas_magic.get_dataframe_from_multiple_csv_files(csv_files)
+xml_dataframes = pandas_magic.get_dataframe_from_multiple_xml_files(xml_files)
+json_dataframe = pandas_magic.get_dataframe_from_multiple_json_files(json_files)
 
 # Concatenate all dataframes
 # all_dataframes = csv_dataframes + xml_dataframes + [json_dataframe]
@@ -18,8 +16,4 @@ json_dataframe = get_dataframe_from_multiple_json_files(json_files)
 # Merge dataframes, drop duplicates, and handle telephone numbers
 # merged_df = pd.concat(all_dataframes, ignore_index=True)
 
-print(csv_dataframe)
-
-# df = pd.read_csv('data/a/b/users_1.csv', delimiter=';', converters={'children': parse_children})
-
-# print(df)
+print(xml_dataframes)
