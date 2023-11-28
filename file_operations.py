7be93449ce9eb974fd37a102e793656c74472b98
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-28 12:53:00 +0100

    upgrade file operation to work with list and single string

--- file_operations.py
+++ file_operations.py
@@ -1,18 +1,22 @@
 from os import walk, path
 
 
-def get_file_list_deep(from_dir, file_ending):
+def get_file_list_deep(from_dir, file_endings):
     """
     Gets all files from the given directory including, accessing every folder below
     :param from_dir: directory from where to start as str
-    :param file_ending: file ending which we are looking for like '.csv'
+    :param file_endings: file ending which we are looking for like '.csv' or list like ['.csv', '.json']
     :return: list of files with given ending
     """
     w = walk(from_dir)
     temp_list = []
+
+    if isinstance(file_endings, str):
+        file_endings = [file_endings]
+
     for (dir_path, dirs, files) in w:
         for file in files:
-            if file.endswith(file_ending):
+            if any(file.endswith(ending) for ending in file_endings):
                 temp_list.append(path.join(dir_path, file).replace(path.sep, '/'))
 
     return temp_list
