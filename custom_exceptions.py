a2f0eb3c31bf4bc2af47f6f7ede75eef211aa595
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-28 09:34:58 +0100

    added custom exceptions for project

---
+++ custom_exceptions.py
@@ -0,0 +1,12 @@
+class CustomError(Exception):
+    """ Base class for custom exceptions """
+
+    def __init__(self, message, code=None, params=None):
+        super().__init__(message, code, params)
+        self.message = message
+        self.code = code
+        self.params = params
+
+
+class InvalidFileException(CustomError):
+    """ Base class for invalid file """
