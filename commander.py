e09ff184535a7f07c56463d253382ac4d460b205
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-29 11:03:39 +0100

    group-by-age implemented

--- commander.py
+++ commander.py
@@ -11,6 +11,7 @@ class Command:
 
 class PrintAllAccountsCommand(Command):
     def execute(self):
+        print(self.data.pandas_dataframe)
         print(len(self.data.pandas_dataframe))
 
 
@@ -24,12 +25,17 @@ class PrintOldestAccountCommand(Command):
 
 class GroupByAgeCommand(Command):
     def execute(self):
-        print("Grouping by age")
+        df_expanded = self.data.pandas_dataframe.explode('children')
+        df_expanded = df_expanded[df_expanded['children'].notna()]
+        df_expanded['age'] = df_expanded['children'].apply(lambda x: x['age'])
+        child_counts = df_expanded['age'].value_counts().sort_values(ascending=True)
+        for age, count in child_counts.items():
+            print(f"age: {age}, count: {count}")
 
 
 class PrintChildrenCommand(Command):
     def execute(self):
-        print("Printing children")
+        pass
 
 
 class FindSimilarChildrenCommand(Command):
