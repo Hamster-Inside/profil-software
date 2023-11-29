2193bc43a9b205452a94bf2298c5b28ae3f44786
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-29 19:30:44 +0100

    cleaning code with pylint

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
-from argparse import ArgumentParser, Namespace, RawTextHelpFormatter, Action, ArgumentError
-from typing import Any
+""" Base file for console application start """
+from argparse import ArgumentParser
 from console_app import ConsoleApp
 from pandas_magic import PandasData
 from file_operations import get_file_list_deep
 
 
-def main(args_from_user):
+def main(app, args_from_user):
+    """ main application, gathering all files from
+    data and parsing it to pandas dataframe to work with methods"""
     files_with_data = get_file_list_deep('data', ['.csv', '.xml', '.json'])
 
     data = PandasData()
@@ -23,16 +25,18 @@ def main(args_from_user):
 
 
 if __name__ == '__main__':
-    app = ConsoleApp()
+    console_app = ConsoleApp()
     parser = ArgumentParser(description='Data collecting App',
                             usage="Type in command line: METHOD --login LOGIN --password PASSWORD")
-    parser.add_argument("command", choices=app.commands.keys(), help="Specify the command to execute")
+    parser.add_argument("command", choices=console_app.commands.keys(),
+                        help="Specify the command to execute")
     parser.add_argument("--login", required=True, help="User login")
     parser.add_argument("--password", required=True, help="User password")
     parser._positionals.title = "Available methods"
     args, unknown_args = parser.parse_known_args()
     if unknown_args:
-        print(f'Error: Nieznane metody: {' '.join(unknown_args)} (Wpisz \"python script.py --help\" w celu pomocy)')
+        print(f'Error: Nieznane metody: {' '.join(unknown_args)} '
+              f'(Wpisz \"python script.py --help\" w celu pomocy)')
     else:
         args = parser.parse_args()
-        main(args)
+        main(console_app, args)
