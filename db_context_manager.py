698e73a8d4df8db013139fd506b52cf5b6a6e741
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-29 17:43:03 +0100

    added simple context manager for db

--- db_context_manager.py
+++ db_context_manager.py
@@ -1,32 +1,19 @@
 import sqlite3
 
 
-class DatabaseConnection:
+class SQLiteDBManager:
+    def __init__(self, db_name='user_data.db'):
+        self.db_name = db_name
+        self.conn = None
 
-    def __init__(self, db_path: str):
-        self.db_path = db_path
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn.cursor()
 
-    def create_connection(self):
-        return sqlite3.connect(self.db_path)
-
-    def create_new_parts_database_file(self, db_file_name):
-        try:
-            conn = sqlite3.connect(db_file_name)
-            cursor = conn.cursor()
-            cursor.execute('''CREATE TABLE parts(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            part_number TEXT NOT NULL,
-            part_quantity INTEGER NOT NULL,
-            finished BOOLEAN,
-            created_at DATE,
-            deadline_date DATE,
-            parts_done_quantity INTEGER NOT NULL DEFAULT '0',
-            email_sent_status BOOLEAN DEFAULT 'FALSE'
-            )''')
-            conn.commit()
-            self.db_path = db_file_name
-        except sqlite3.Error as e:
-            print(e)
-        finally:
-            if conn:
-                conn.close()
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            if exc_type is None:
+                self.conn.commit()  # Commit changes if no exception occurred
+            else:
+                self.conn.rollback()  # Rollback changes if an exception occurred
+            self.conn.close()
