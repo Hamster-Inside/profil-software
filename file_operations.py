12162222fbe2ec4d99a768a18b2cfec67e7149c8
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-29 19:45:23 +0100

    cleaning code with pylint

--- file_operations.py
+++ file_operations.py
@@ -1,3 +1,4 @@
+""" Custom module for operations on files """
 from os import walk, path
 
 
@@ -5,7 +6,8 @@ def get_file_list_deep(from_dir, file_endings):
     """
     Gets all files from the given directory including, accessing every folder below
     :param from_dir: directory from where to start as str
-    :param file_endings: file ending which we are looking for like '.csv' or list like ['.csv', '.json']
+    :param file_endings: file ending which we are looking for
+            like '.csv' or list like ['.csv', '.json']
     :return: list of files with given ending
     """
     w = walk(from_dir)
@@ -14,7 +16,7 @@ def get_file_list_deep(from_dir, file_endings):
     if isinstance(file_endings, str):
         file_endings = [file_endings]
 
-    for (dir_path, dirs, files) in w:
+    for (dir_path, _, files) in w:
         for file in files:
             if any(file.endswith(ending) for ending in file_endings):
                 temp_list.append(path.join(dir_path, file).replace(path.sep, '/'))
